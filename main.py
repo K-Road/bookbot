29d1955a6d4034a5ab5313ae1e432ecac37d3b5c
K-Road <game@chrodeprime.com>
2023-12-15 20:52:09 +1100

    add print report main.py

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    char_dict = get_char_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(char_dict)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def sort_on(d):
+    return d["num"]
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_char_dict(string):
+    chars = {}
+    for c in string:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
 main()
